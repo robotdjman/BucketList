537880bb48f7bf2502acdb8b7205a3ac9f61f2ab
Rylan Metcalfe <robotdjman12@gmail.com>
2018-11-30 12:00:01 -0700

    Initial commit

---
+++ BucketList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BucketList
+//
+//  Created by Metcalfe, Rylan on 11/30/18.
+//  Copyright © 2018 Metcalfe, Rylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
