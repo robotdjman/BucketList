aa9c574d3bc597c2aa053eeaf439c333677ae100
Rylan Metcalfe <robotdjman12@gmail.com>
2018-11-30 12:31:08 -0700

    Step 1

---
+++ BucketList/Model/BucketItem.swift
@@ -0,0 +1,23 @@
+//
+//  BucketItem.swift
+//  BucketList
+//
+//  Created by Metcalfe, Rylan on 11/30/18.
+//  Copyright © 2018 Metcalfe, Rylan. All rights reserved.
+//
+
+import Foundation
+
+public class BucketItem
+{
+    let itemContents : String
+    let itemAuthor : String
+    
+    init(contents : String, author : String)
+    
+    {
+        itemContents = contents
+        itemAuthor = author
+    }
+    
+}
